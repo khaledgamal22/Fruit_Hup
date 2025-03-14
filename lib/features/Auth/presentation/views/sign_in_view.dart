742ecdd4cfb5c8f412dd486a31cd31e3ec3ca7f2
khaledgamal22 <kghaly56@gmail.com>
2025-03-14 16:46:29 +0200

    edit auth

--- lib/features/Auth/presentation/views/sign_in_view.dart
+++ lib/features/Auth/presentation/views/sign_in_view.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fruits_ecommerce_app/core/helper_functions/build_custom_appBar.dart';
 import 'package:fruits_ecommerce_app/core/helper_functions/get_it_func.dart';
 import 'package:fruits_ecommerce_app/features/Auth/domain/repos/sign_in_repo.dart';
 import 'package:fruits_ecommerce_app/features/Auth/presentation/view_models/cubits/sign_in/sign_in_cubit.dart';
 
+import '../../../../uitilits/app_style.dart';
 import 'widgets/sign_in_view_body_bloc_consumer.dart';
 
 class SignInView extends StatelessWidget {
@@ -17,7 +16,16 @@ class SignInView extends StatelessWidget {
       create: (context) => SignInCubit(getIt.get<SignInRepo>()),
       child: SafeArea(
         child: Scaffold(
-          appBar: buildCustomAppBar(context, 'تسجيل دخول', null),
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.white,
+            leadingWidth: 80,
+            title: Text(
+              'تسجيل الدخول',
+              style: AppStyle.styleBold19(context),
+            ),
+            centerTitle: true,
+          ),
           body: const SignInViewBodyBlocConsumer(),
         ),
       ),
--- lib/features/Auth/presentation/views/sign_up_view.dart
+++ lib/features/Auth/presentation/views/sign_up_view.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fruits_ecommerce_app/core/helper_functions/build_custom_appBar.dart';
 import 'package:fruits_ecommerce_app/core/helper_functions/get_it_func.dart';
 import 'package:fruits_ecommerce_app/features/Auth/domain/repos/sign_up_repo.dart';
 import 'package:fruits_ecommerce_app/features/Auth/presentation/view_models/cubits/sign_up/sign_up_cubit.dart';
 
+import '../../../../uitilits/app_style.dart';
 import 'widgets/sign_up_view_body_bloc_consumer.dart';
 
 class SignUpView extends StatelessWidget {
@@ -18,7 +18,15 @@ class SignUpView extends StatelessWidget {
       ),
       child: SafeArea(
         child: Scaffold(
-          appBar: buildCustomAppBar(context, 'حساب جديد', null),
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            leadingWidth: 80,
+            title: Text(
+              'حساب جديد',
+              style: AppStyle.styleBold19(context),
+            ),
+            centerTitle: true,
+          ),
           body: const SignUpViewBodyBlocConsumer(),
         ),
       ),
