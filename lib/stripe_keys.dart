7fc65f9d4d28a3188bb05f31f90118d298219dcb
khaledgamal22 <kghaly56@gmail.com>
2025-03-18 06:20:30 +0200

    Removed sensitive file and added to .gitignore

--- lib/stripe_keys.dart
+++
@@ -1,6 +0,0 @@
-class StripeKeys {
-  static const String publishableKey =
-      'pk_test_51PuzFU00LLpq2Gecj8z0mSJp85sucJkGayUzOZRFGUj2yElSYvilNjvpidrmOBqXB8hsvFPddxP4yhrlmU0QMXQ700BNgFE4OL';
-  static const String secertKey =
-      'sk_test_51PuzFU00LLpq2GecyywrouJdTLuRu3CiiriDRBhf5kv116Y77HE0MuI5UbZm8RN8M4mDLiFABWAELW0ltLzYMikZ00dp3aXxOJ';
-}
